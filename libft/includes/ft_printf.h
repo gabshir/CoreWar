df25e451b483490d7fcb576faffcce74bb363af0
Jessika Willem frey <jwillem-@oa-c1.21-school.ru>
2019-05-27 20:18:00 +0300

    printf

--- libft/includes/ft_printf.h
+++ libft/includes/ft_printf.h
@@ -5,99 +5,99 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: jwillem- <jwillem-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/01/07 13:57:16 by jwillem-          #+#    #+#             */
-/*   Updated: 2019/05/15 13:37:08 by gabshire         ###   ########.fr       */
+/*   Created: 2019/02/23 03:47:46 by gabshire          #+#    #+#             */
+/*   Updated: 2019/05/27 14:30:35 by jwillem-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
-# define POWER(precision)	(precision > 48 ? 48 : precision)
-# define PREC_F(precision)	(precision <= -1 ? 6 : precision)
-# define PREC(precision)	(precision <= -1 ? 0 : precision)
-# define PREC_A(precision)	(precision <= -1 ? 13 : precision)
-# define STRIFNEG(num)		(num < 0 ? "-0x" : "0x")
-# define SIGNOFEX(ex)		(ex >= 0 ? "+" : "-")
-
-# include <stdarg.h>
 # include "libft.h"
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+# include <stdarg.h>
+# include <wchar.h>
+# include <limits.h>
+# include <stdlib.h>
 
-int				g_len;
+# define MAX_BUF_SIZE	100
+# define NINF c == c + c / .0 && c < 0
+# define INF c == c + c / .0 && c > 0
 
-typedef struct	s_specs
+typedef struct				s_format
 {
-	int		flags[5];
-	int		width;
-	int		precision;
-	int		size;
-	char	type;
-}				t_specs;
+	char			*str;
+	va_list			arg;
+	int				hp;
+	int				t;
+	char			sr[3];
+	int				len;
+	unsigned char	tp;
+	unsigned char	hps : 1;
+	unsigned char	st : 1;
+	unsigned char	m : 1;
+	unsigned char	pl : 1;
+	unsigned char	pr : 1;
+	unsigned char	o : 1;
+	unsigned char	z : 1;
+	char			buf[MAX_BUF_SIZE];
+	int				b_i;
+}							t_format;
 
-typedef enum	e_size
+typedef struct				s_format_d
 {
-	hh = 1,
-	h,
-	l,
-	L,
-	ll,
-	j,
-	z,
-	t
-}				t_size;
+	unsigned long long int	o;
+	size_t					spase;
+	size_t					zero;
+	size_t					minus;
+	size_t					plus;
+	size_t					base;
+	int						len;
+	size_t					rd;
+}							t_format_d;
 
-typedef enum	e_flag
-{
-	sharp,
-	zero,
-	minus,
-	plus,
-	space
-}				t_flag;
+int							ft_printf(char const *s, ...);
+void						ft_flag(t_format *mod);
+void						ft_tochnost(t_format *mod);
+void						ft_hirina(t_format *mod);
+void						ft_specr(t_format *mod);
+void						write_and_zero(t_format *p);
+void						ft_restor_p(t_format *p);
 
-t_specs			*create_new_specs(void);
+size_t						ft_start(t_format *mod);
+size_t						ft_char(t_format *mod);
+size_t						ft_printstr(t_format *mod);
+void						ft_simup(t_format *p, size_t len, char c);
+size_t						kol_sim(unsigned long long int d, size_t base);
+void						itua_base_mod(t_format_d *d, t_format *p, size_t f);
 
-void			find_start_specifier(char *format, va_list *ap);
+/*
+** printnbr int
+*/
 
-char			*find_all_specifier(char *start_ptr, t_specs *specs, \
-				va_list *ap);
-char			*find_size_specifier(char *start_ptr, t_specs *specs);
-char			*find_type_specifier(char *start_ptr, t_specs *specs);
-char			*find_widht_specifier(char *start_ptr, t_specs *specs, \
-				va_list *ap);
-char			*find_precision_specifier(char *start_ptr, t_specs *specs, \
-				va_list *ap);
-char			*find_flag_specifier(char *start_ptr, t_specs *specs);
+size_t						incd(t_format *mod, int len, int f, size_t n);
+void						ft_format_d(t_format *p, t_format_d *d, int f);
+size_t						ft_printnbr(t_format *p, long long k);
+size_t						ft_printnbru(t_format *p, unsigned long long k);
+size_t						ft_flag_d_i(t_format *p);
+size_t						ft_flag_u(t_format *p);
 
-char			*get_value(char *start_ptr, t_specs *specs, va_list *ap);
-void			print_value(t_specs *specs, char *val, int len_val);
-char			*re_val(char *val, int *sign);
-char			*use_sval(t_specs *specs, char *val, long long nbr);
-char			*use_uval(t_specs *specs, char *val, unsigned long long nbr);
+/*
+** print x
+*/
 
-char			*handling_precision(t_specs *specs, char *val, int len);
-char			*handling_zero(t_specs *specs, char *val, int len);
-char			*handling_size(t_specs *specs, unsigned long long nb, int base);
-char			*filling_zero(char *val, int len);
+size_t						ft_flag_x(t_format *mod);
+size_t						ft_printx(t_format *p,
+		unsigned long long int k, int f);
 
-void			print_type_di(t_specs *specs, va_list *ap);
-void			print_type_c(t_specs *specs, va_list *ap);
-void			print_type_s(t_specs *specs, va_list *ap);
-void			print_type_p(t_specs *specs, va_list *ap);
-void			print_type_oux(t_specs *specs, va_list *ap);
-void			print_type_percent(t_specs *specs);
-void			print_type_f(t_specs *specs, va_list *ap);
-void			print_type_e(t_specs *specs, va_list *ap);
-void			print_type_a(t_specs *specs, va_list *ap);
-void			print_type_g(t_specs *specs, va_list *ap);
-void			print_type_n(va_list *ap);
-char			*print_non_spec(char *start_ptr, t_specs *specs);
-
-char			*ft_dtoa_base(double num, int precision, int base);
-char			*ft_frac_base(double int_frac, int precision, int base);
-char			*ft_dtoa_e(double num, t_specs *specs);
-char			*del_last_zeros(char *val, t_specs *specs);
-double			check_null_prec_dtoa_e(double num);
-void			rounding(char *str, int i);
-
+size_t						ft_charu(t_format *p, wint_t c);
+size_t						ft_printstru(t_format *p);
+size_t						ft_float(t_format *p);
+size_t						ft_floatlong(t_format *p);
+void						ft_sborfloat(t_format *p, t_format_d *d,
+		int f, char *okr);
+void						ft_incf(t_format *p, t_format_d *d, int f);
+size_t						ft_nan(t_format *p, double c);
 #endif
