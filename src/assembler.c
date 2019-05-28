dd2a4e9b24be7672796d26c72ae73c302a0c339d
Galazza galare Abshire <gabshire@oa-d2.21-school.ru>
2019-05-28 16:15:19 +0300

    up algo

--- src/assembler.c
+++ src/assembler.c
@@ -6,28 +6,49 @@
 /*   By: gabshire <gabshire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/27 18:37:16 by gabshire          #+#    #+#             */
-/*   Updated: 2019/05/28 14:13:00 by gabshire         ###   ########.fr       */
+/*   Updated: 2019/05/28 16:11:42 by gabshire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 #include "op.h"
 
-int	ft_strncmpm(char *s1, char *s2)
+size_t quick_pass(char *line)
 {
-	int len;
+	size_t i;
 
-	len  = 0;
-	while (s1[len] && (s1[len] == ' ' || s1[len] == '\t'))
-		++len;
-	while (s1[len] && *s2)
+	i = 0;
+	while (line[i] == ' ' || line[i] =='\t')
+		++i;
+	return (i);
+}
+
+char	*checkform(int fd)
+{
+	char	*line;
+	size_t	i;
+
+	line = NULL;
+	while (get_next_line(fd, &line))
 	{
-		if (s1[len] != *s2)
-			return (-1);
-		++len;
-		++s2;
+		i = 0;
+		while (line[i])
+		{
+			if (line[i] == COMMENT_CHAR || line[i] == ALT_COMMENT_CHAR)
+			{
+				free(line);
+				line = NULL;
+				break ;
+			}
+			if (line[i] == ' ' || line[i] == '\t')
+				++i;
+			else
+				return (line);
+		}
+		free(line);
+		line = NULL;
 	}
-	return (len);
+	return (NULL);
 }
 
 void	checkname(int fd, header_t *chemp)
@@ -36,24 +57,22 @@ void	checkname(int fd, header_t *chemp)
 	int		i;
 	int		j;
 
-	line = NULL;
 	i = 0;
-	get_next_line(fd, &line);
-	!line ? ft_eroror(0) : 0;
-	j = ft_strncmpm(line, NAME_CMD_STRING);
-	j == -1 ? ft_eroror(1) : 0;
+	line = checkform(fd);
+	!line ? ft_error(0) : 0;
+	j == -1 ? ft_error(1) : 0;
 	while (line[j] && line[j] == ' ' && line[j] == '\t')
 		++j;
-	line[j] ? ++j : ft_eroror(1);
-	line[j] && line[j] == '"' ? ++j : ft_eroror(1);
+	line[j] ? ++j : ft_error(1);
+	line[j] && line[j] == '"' ? ++j : ft_error(1);
 	while(i < PROG_NAME_LENGTH && line[j] && line[j] != '"')
 	{
 		chemp->prog_name[i] = line[j];
 		++i;
 		++j;
 	}
-	i <= PROG_NAME_LENGTH && line[j] != '"' ? ft_eroror(1) : ++j;
-	line[j] ? ft_eroror(1) : 0;
+	i <= PROG_NAME_LENGTH && line[j] != '"' ? ft_error(1) : ++j;
+	line[j] ? ft_error(1) : 0;
 	free(line);
 	line = NULL;
 }
